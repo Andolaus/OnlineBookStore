d2803e0fdfcca90c9d3425db1d0a31cc5a309f7b
Andolaus <andolaus@gmail.com>
2025-01-26 12:31:34 +0100

    Lagt til exceptions i BookCollections

--- Collections/BookCollection.cs
+++ Collections/BookCollection.cs
@@ -9,26 +9,58 @@ namespace OnlineBookStore.Collections
 
         public void AddBook (Book book)
         {
+            if (book == null)
+            {
+                throw new ArgumentNullException(nameof(book), "Book cannot be null");
+            }
+
+            if (_books.Any(b => b.Isbn == book.Isbn))
+            {
+                throw new ArgumentException("Book with same ISBN already exists", nameof(book));
+            }
+
             _books.Add(book);
         }
 
         public void RemoveBook (Book book)
         {
+            if (book == null)
+            {
+                throw new ArgumentNullException(nameof(book), "Book cannot be null");
+            }
+
+            if (!_books.Contains(book))
+            {
+                throw new ArgumentException("Book doesn't exist in collection, failed to remove book", nameof(book));
+            }
+
             _books.Remove(book);
         }
 
         public int BookCount () => _books.Count;
 
-        public List<Book> GetBooks () => _books;
+        public List<Book> GetBooks () => new List<Book>(_books);
 
         public Book FindBookByIsbn (string isbn) 
         {
-            return _books.FirstOrDefault(book => book.Isbn == isbn);
+            if (string.IsNullOrWhiteSpace(isbn))
+            {
+                throw new ArgumentException("ISBN cannot be null or empty", nameof(isbn));
+            }
+            
+            return _books.FirstOrDefault(book => book.Isbn == isbn)
+                ?? throw new ArgumentException("Book with that ISBN not found", nameof(isbn));
         }
 
         public Book FindBookByTitle (string title)
         {
-            return _books.FirstOrDefault(book => book.Title == title);
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                throw new ArgumentException("Title cannot be null or empty", nameof(title));
+            }
+
+            return _books.FirstOrDefault(book => book.Title == title)
+                ?? throw new ArgumentException("Book with that title does not exist", nameof(title));
         }
     }
 }
