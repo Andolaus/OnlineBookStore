fde8de824cb213eb34257d9f9a66e841937101fa
Andolaus <andolaus@gmail.com>
2025-01-30 14:28:05 +0100

    Created a discount manager class

---
+++ Managers/DiscountManager.cs
@@ -0,0 +1,31 @@
+using OnlineBookStore.Models;
+
+namespace OnlineBookStore.Managers 
+{
+    /// <summary>
+    /// Class representing a discount manager for a online book store.
+    /// </summary>
+    public class DiscountManager 
+    {
+        /// <summary>
+        /// Calculates discount if book is on sale.
+        /// </summary>
+        /// <param name="book">The book to check if it's on sale.</param>
+        /// <returns>Discounted price if sale.</returns>
+        /// <exception cref="ArgumentNullException">Throws if book is null.</exception>
+        public decimal CalculateDiscountedPrice(Book book)
+        {
+            if (book == null)
+            {
+                throw new ArgumentNullException("Book cannot be null", nameof(book));
+            }
+
+            if (book.IsBookOnSale)
+            {
+                return book.Price * 0.8m;
+            }
+
+            return book.Price;
+        }
+    }
+}
